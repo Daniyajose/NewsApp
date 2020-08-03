b43153b19356be023379c89eb511e96c9b42d4fa
Daniyajose <josedaniya@gmail.com>
2020-08-03 11:33:12 -0700

    getdata method added in main activity

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getData()
+    }
+
+    private fun getData() {
+       Log.e("MainActivity","getData method called")
     }
 }
